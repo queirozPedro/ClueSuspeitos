e79a2a78df91c35d71702fc141bf239984617641
Pedro Vinícius <pedrovinic12@gmail.com>
2024-06-25 14:26:45 -0300

    Criando a geração das tabelas em um arquivo txt

--- PythonTableGenerator/table_generator.py
+++ PythonTableGenerator/table_generator.py
@@ -83,78 +83,97 @@ def gerar_tabela(jogadores):
     tabela_jogador3[mascara] = 1  
     tabela_jogador3[~mascara] = -1  
     
-    tabela_geral = np.array([tabela_jogador0, tabela_jogador1, tabela_jogador2, tabela_jogador3])
+    tabela = np.array([tabela_jogador0, tabela_jogador1, tabela_jogador2, tabela_jogador3])
 
-    return tabela_geral
+    return tabela
 
-def imprimir_tabela(tabela_geral):
-    tabela = ''
+def gerar_string_tabela(tabela):
+    string = ''
     for i in range(15):
         if i == 0:
-            tabela += f'Castiçal({i+1})         | '
+            string += f'Castical({i+1})         | '
         elif i == 1:
-            tabela += f'Corda({i+1})            | '
+            string += f'Corda({i+1})            | '
         elif i == 2:
-            tabela += f'Faca({i+1})             | '
+            string += f'Faca({i+1})             | '
         elif i == 3:
-            tabela += f'Revólver({i+1})         | '
+            string += f'Revolver({i+1})         | '
         elif i == 4:
-            tabela += f'Cozinha({i+1})          | '
+            string += f'Cozinha({i+1})          | '
         elif i == 5:
-            tabela += f'Hall({i+1})             | '
+            string += f'Hall({i+1})             | '
         elif i == 6:
-            tabela += f'Sala de Estar({i+1})    | '
+            string += f'Sala de Estar({i+1})    | '
         elif i == 7:
-            tabela += f'Sala de Jantar({i+1})   | '
+            string += f'Sala de Jantar({i+1})   | '
         elif i == 8:
-            tabela += f'Spa({i+1})              | '
+            string += f'Spa({i+1})              | '
         elif i == 9:
-            tabela += f'Green({i+1})           | '
+            string += f'Green({i+1})           | '
         elif i == 10:
-            tabela += f'Mustard({i+1})         | '
+            string += f'Mustard({i+1})         | '
         elif i == 11:
-            tabela += f'Peacock({i+1})         | '
+            string += f'Peacock({i+1})         | '
         elif i == 12:
-            tabela += f'Plum({i+1})            | '
+            string += f'Plum({i+1})            | '
         elif i == 13:
-            tabela += f'Scarlet({i+1})         | '
+            string += f'Scarlet({i+1})         | '
         elif i == 14:
-            tabela += f'White({i+1})           | '
+            string += f'White({i+1})           | '
         for j in range(4):
-            if tabela_geral[j][i] == 1 or tabela_geral[j][i] == 0:
-                tabela += ' '+ str(tabela_geral[j][i])+ " | "
+            if tabela[j][i] == 1 or tabela[j][i] == 0:
+                string += ' '+ str(tabela[j][i])+ " | "
             else:
-                tabela += str(tabela_geral[j][i])+ " | "
+                string += str(tabela[j][i])+ " | "
                         
-        tabela += '\n'
-    print(tabela)
+        string += '\n'
+    return(string)
 
-def corromper_tabela(tabela_geral, chance1, chance_menos1):
+def corromper_tabela(tabela, chance1, chance_menos1):
     for i in range(15):
         for j in range(1,4):
-            if tabela_geral[j][i] == 1:
-                if np.random.randint(0, 101) < chance1:
-                    tabela_geral[j][i] = 0
-            elif tabela_geral[j][i] == -1:
-                if np.random.randint(0, 101) < chance_menos1:
-                    tabela_geral[j][i] = 0
-    return tabela_geral;
+            if tabela[j][i] == 1:
+                if np.random.randint(0, 100) < chance1:
+                    tabela[j][i] = 0
+            elif tabela[j][i] == -1:
+                if np.random.randint(0, 100) < chance_menos1:
+                    tabela[j][i] = 0
+    return tabela;
 
+def gerar_tabelas_txt():
+    print("Caracteristicas do Arquivo")
+    quant_tabelas = int(input("Quantidade de Tabelas: "))
+    chance1 = int(input("Probabilidade de 1 ser corrompido(0 a 100): "))
+    chance_menos1 = int(input("Probabilidade de -1 ser corrompido(0 a 100): "))
+    nome_arquivo = f'{quant_tabelas}_jogos_com_{chance1}%_para_1_e_{chance_menos1}%_para_-1.txt'
+    with open(nome_arquivo, 'w') as arquivo:
+        for i in range(quant_tabelas):
+
+            crime = gerar_crime()
+            evidencias = gerar_evidencias(crime)
+            jogadores = gerar_jogadores(evidencias)
+            tabela = corromper_tabela(gerar_tabela(jogadores), chance1, chance_menos1)
+
+            linhas = [
+                f'Jogo {i+1} de {quant_tabelas}\n',
+                f'{chance1}% de chance de corromper o 1\n',
+                f'{chance_menos1}% de chance de corromper o -1\n\n',
+                f'Crime: {crime}\n',
+                f'Cartas dos Jogadores\n',
+                f'  Jogador {0}: {jogadores[0]}\n',
+                f'  Jogador {1}: {jogadores[1]}\n',
+                f'  Jogador {2}: {jogadores[2]}\n',
+                f'  Jogador {3}: {jogadores[3]}\n\n',
+                f'Tabela Corrompida \n',
+                gerar_string_tabela(tabela),
+                '\n\n\n'
+            ]
+
+            arquivo.writelines(linhas)
+    print("Arquivo Gerado com Sucesso")
 
 def main():   
-    crime = gerar_crime()
-    evidencias = gerar_evidencias(crime)
-    jogadores = gerar_jogadores(evidencias)
-    tabela_geral = gerar_tabela(jogadores)
-
-    print("Crime:\t", crime, "\n")
-    print("Jogadores:\n", jogadores, '\n')
-    print('Tabela\n')
-    imprimir_tabela(tabela_geral)
-
-    tabela_corrompida = corromper_tabela(tabela_geral, 10, 10)
-    print('Tabela Corrompida\n')
-    imprimir_tabela(tabela_corrompida)
+    gerar_tabelas_txt()
 
 if __name__ == "__main__":
     main()
