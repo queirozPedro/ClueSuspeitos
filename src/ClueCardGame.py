654072fde7fcc570b832d0f024cc9780d4a8f22b
queirozPedro <pedrovinic12@gmail.com>
2024-08-19 16:36:01 -0300

    Criando método que faz os treinos e exibe em um Diagrama de Venn

--- src/ClueCardGame.py
+++ src/ClueCardGame.py
@@ -1,4 +1,6 @@
 from sklearn.neural_network import MLPClassifier
+from matplotlib_venn import venn3, venn3_circles
+from matplotlib import pyplot as plt
 import numpy as np
 
 '''
@@ -407,7 +409,7 @@ def iniciar_treino():
         f"Para um total de {quant_testes} testes automáticos e aleatórios\n",
         f"Com chance de {chance1}% de Corromper o 1\n",
         f"Com chance de {chance_menos1}% de Corromer o -1\n",
-        f"\Resultado\n",
+        f"Resultado\n",
         f"Acertou tudo: {(acertos/quant_testes)*100:.2f}%\n",
         f"Acertou somente Arma e Lugar: {(acertos_arma_lugar/quant_testes)*100:.2f}%\n",
         f"Acertou somente Arma e Suspeito: {(acertos_arma_suspeito/quant_testes)*100:.2f}%\n",
@@ -420,8 +422,155 @@ def iniciar_treino():
 
     print("".join(string))
 
+def iniciar_treino_venn():
+    print(f"\n\nCaracteristicas do Treino")
+    quant_jogos_entrada = int(input("Quantidade de Jogos para Treinamento: "))
+    quant_testes = int(input("Quantidade de Palpites Automáticos: "))
+    print(f"Informações adicionais")
+    chance1 = int(input("Chance de Corromper o 1: "))
+    chance_menos1 = int(input("Chance de Corromper o -1: "))
+
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 3), random_state=1, max_iter=1000000)
+    clf1 = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 3), random_state=1, max_iter=1000000)
+    clf2 = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 3), random_state=1, max_iter=1000000)
+
+    # Os itens nomeados com X serão as tabelas com 0, 1 e -1 referentes aos jogadores.
+    arma_X = []
+    lugar_X = []
+    suspeito_X = []
+    # Os itens nomeados com y serão as respostas dos crimes.
+    arma_y = []
+    lugar_y = []
+    suspeito_y = []
+
+    # Base de dados
+    for n in range(quant_jogos_entrada):
+        # Vou criar o crime e a tabela
+        crime, tabela = criar_cenario(chance1, chance_menos1)
+        # Distribuir o crime em seu respectivos y
+        arma_y.append(crime[0])
+        lugar_y.append(crime[1])
+        suspeito_y.append(crime[2])
+
+        # Divide um array para cada tipo de carta
+        # Um array com as informações de armas
+        tabela_aux = []
+        for i in range(4):
+            for j in range(4):
+                tabela_aux.append(tabela[i][j])
+        arma_X.append(tabela_aux)
+        # Um array com as informações de lugares
+        tabela_aux = []
+        for i in range(4):
+            for j in range(4,9):
+                tabela_aux.append(tabela[i][j])
+        lugar_X.append(tabela_aux)
+        # Um array com as informações de suspeitos
+        tabela_aux = []
+        for i in range(4):
+            for j in range(9,15):
+                tabela_aux.append(tabela[i][j])
+        suspeito_X.append(tabela_aux)
+
+    # 'Cria' ou 'faz' os treinos
+    clf.fit(arma_X, arma_y)
+    clf1.fit(lugar_X, lugar_y)
+    clf2.fit(suspeito_X, suspeito_y)
+
+    # Todas as possibilidades de acertos e erros
+    acertos = 0
+    acertos_arma = 0
+    acertos_lugar = 0
+    acertos_suspeito = 0
+    erros_totais = 0
+    acertos_arma_lugar = 0
+    acertos_arma_suspeito = 0
+    acertos_lugar_suspeito = 0
+
+    for m in range(quant_testes):
+        crime, tabela = criar_cenario(chance1, chance_menos1)
+
+        tabela_arma = []
+        for i in range(4):
+            for j in range(4):
+                tabela_arma.append(tabela[i][j])
+
+        tabela_lugar = []
+        for i in range(4):
+            for j in range(4,9):
+                tabela_lugar.append(tabela[i][j])
+
+        tabela_suspeito = []
+        for i in range(4):
+            for j in range(9,15):
+                tabela_suspeito.append(tabela[i][j])
+
+        palpite_arma = (clf.predict([tabela_arma]))
+        palpite_lugar = (clf1.predict([tabela_lugar]))
+        palpite_suspeito = (clf2.predict([tabela_suspeito]))
+
+        if crime[0] == palpite_arma: # Arma
+            if crime[1] == palpite_lugar: # Arma e Lugar
+                if crime[2] == palpite_suspeito: # Arma, lugar e suspeito
+                    acertos += 1
+                else:
+                    acertos_arma_lugar += 1
+            elif crime[2] == palpite_suspeito: # Arma e suspeito
+                acertos_arma_suspeito += 1
+            else:
+                acertos_arma += 1
+        elif crime[1] == palpite_lugar: # Lugar
+            if crime[2] == palpite_suspeito: # Lugar e suspeito
+                acertos_lugar_suspeito += 1
+            else:
+                acertos_lugar += 1
+        elif crime[2] == palpite_suspeito: # Suspeito
+            acertos_suspeito += 1
+        else:
+            erros_totais += 1
+
+    # # Vou deixar comentado aqui só para caso queira tirar a prova
+    # string = [
+    #     f"\n\nCom um total de {quant_jogos_entrada} entradas de jogos\n",
+    #     f"Para um total de {quant_testes} testes automáticos e aleatórios\n",
+    #     f"Com chance de {chance1}% de Corromper o 1\n",
+    #     f"Com chance de {chance_menos1}% de Corromer o -1\n",
+    #     f"Resultado\n",
+    #     f"Acertou tudo: {(acertos/quant_testes)*100:.2f}%\n",
+    #     f"Acertou somente Arma e Lugar: {(acertos_arma_lugar/quant_testes)*100:.2f}%\n",
+    #     f"Acertou somente Arma e Suspeito: {(acertos_arma_suspeito/quant_testes)*100:.2f}%\n",
+    #     f"Acertou somente Lugar e Suspeito: {(acertos_lugar_suspeito/quant_testes)*100:.2f}%\n",
+    #     f"Acertou apenas a Arma: {(acertos_arma/quant_testes)*100:.2f}%\n",
+    #     f"Acertou apenas o Lugar: {(acertos_lugar/quant_testes)*100:.2f}%\n",
+    #     f"Acertou apenas o Suspeito: {(acertos_suspeito/quant_testes)*100:.2f}%\n",
+    #     f"Errou tudo: {(erros_totais/quant_testes)*100:.2f}%\n"
+    # ]
+    # print("".join(string))
+
+    venn3(subsets=(round((acertos_arma/quant_testes)*100, 2), 
+                   round((acertos_lugar/quant_testes)*100, 2), 
+                   round((acertos_arma_lugar/quant_testes)*100, 2), 
+                   round((acertos_suspeito/quant_testes)*100, 2),
+                   round((acertos_arma_suspeito/quant_testes)*100, 2), 
+                   round((acertos_lugar_suspeito/quant_testes)*100, 2),
+                   round((acertos/quant_testes)*100, 2)),
+          set_labels=("Arma", "Lugar", "Suspeito"),
+          set_colors=("orange", "blue", "red"))
+    
+    venn3_circles(subsets=(round((acertos_arma/quant_testes)*100, 2), 
+                   round((acertos_lugar/quant_testes)*100, 2), 
+                   round((acertos_arma_lugar/quant_testes)*100, 2), 
+                   round((acertos_suspeito/quant_testes)*100, 2),
+                   round((acertos_arma_suspeito/quant_testes)*100, 2), 
+                   round((acertos_lugar_suspeito/quant_testes)*100, 2),
+                   round((acertos/quant_testes)*100, 2)))
+    
+    plt.title(f"Resultado do treino (em %)")
+    plt.text(0.6, -0.6, f"Erros: {round((acertos/quant_testes)*100, 2)}", fontsize=12)
+    plt.show()
+
 def main(): 
-    iniciar_treino()
+    iniciar_treino_venn()
 
 
 if __name__ == "__main__":
