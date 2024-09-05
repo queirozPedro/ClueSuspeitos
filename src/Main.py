c3444afa88bb8429bc3c845b29e141b7ad4e8d93
queirozPedro <pedrovinic12@gmail.com>
2024-09-05 11:32:44 -0300

    Organizando funções e nomes de variáveis

--- src/Main.py
+++ src/Main.py
@@ -10,37 +10,13 @@ from Scene import *
 from SaveToFile import *
 from Table import *
 
-
-def treinar(entradas, jogos_teste, chance1, chance_menos1):
-
-    clf_arma = MLPClassifier(hidden_layer_sizes=(16, 8), activation='relu', solver='adam', alpha=0.0001, max_iter=1000000, random_state=1)
-    clf_lugar = MLPClassifier(hidden_layer_sizes=(20, 10), activation='relu', solver='adam', alpha=0.0001, max_iter=1000000, random_state=1)
-    clf_suspeito = MLPClassifier(hidden_layer_sizes=(24, 12), activation='relu', solver='adam', alpha=0.0001, max_iter=1000000, random_state=1)
-
-    param_grid_arma = {
-        'hidden_layer_sizes': (16, 8),  # [(50,50), (100,)]
-        'activation': ['relu'],
-        'solver': ['adam'],               #, 'sgd'],
-    }
-    param_grid_lugar = {
-        'hidden_layer_sizes': (20, 10), # [(50,50), (100,)]
-        'activation': ['relu'],           #,
-        'solver': ['adam'],                   #['adam', 'sgd'],
-    }
-    param_grid_suspeito = {
-        'hidden_layer_sizes': (24, 12), # [(50,50), (100,)]
-        'activation': ['relu'],
-        'solver': ['adam'],                   #['adam', 'sgd'],
-    }
-    
-    grid_search_arma = GridSearchCV(estimator=clf_arma, param_grid=param_grid_arma, n_jobs=-1, cv=3)
-    grid_search_lugar = GridSearchCV(estimator=clf_lugar, param_grid=param_grid_lugar, n_jobs=-1, cv=3)
-    grid_search_suspeito = GridSearchCV(estimator=clf_suspeito, param_grid=param_grid_suspeito, n_jobs=-1, cv=3)
-
-    # clf_arma = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(8, 4), random_state=1, max_iter=1000000)
-    # clf_lugar = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(10, 5), random_state=1, max_iter=1000000)
-    # clf_suspeito = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(12, 6), random_state=1, max_iter=1000000)
-
+def gerar_dados(dados, chance1, chance_menos1):
+    '''
+    Função responsável por gerar a base de dados usada no treino.
+    Recebe a quantidade de dados que devem ser gerados, a chance do
+    número 1 ser corrompido e a chance do número -1 ser corrompido.
+    Retorna X seguido de y para cada tipo de carta.
+    '''
     # Os itens nomeados com X serão as tabelas com 0, 1 e -1 referentes aos jogadores.
     arma_X = []
     lugar_X = []
@@ -51,7 +27,7 @@ def treinar(entradas, jogos_teste, chance1, chance_menos1):
     suspeito_y = []
 
     # Base de dados
-    for n in range(entradas):
+    for n in range(dados):
         # Vou criar o crime e a tabela
         crime, tabela = criar_cenario(chance1, chance_menos1)
         # Distribuir o crime em seu respectivos y
@@ -79,10 +55,47 @@ def treinar(entradas, jogos_teste, chance1, chance_menos1):
                 tabela_aux.append(tabela[i][j])
         suspeito_X.append(tabela_aux)
 
-    # # 'Cria' ou 'faz' os treinos
-    # clf_arma.fit(arma_X, arma_y)
-    # clf_lugar.fit(lugar_X, lugar_y)
-    # clf_suspeito.fit(suspeito_X, suspeito_y)
+    return (arma_X, arma_y, lugar_X, lugar_y, suspeito_X, suspeito_y)
+
+def definir_param_grid():
+    '''
+    Função que retorna os param_grid de arma, lugar e suspeito
+    '''
+    param_grid_arma = {
+        'hidden_layer_sizes': (8, 4),  # [(50,50), (100,)]
+        'activation': ['relu'],
+        'solver': ['adam'],               #, 'sgd'],
+    }
+    param_grid_lugar = {
+        'hidden_layer_sizes': (10, 5), # [(50,50), (100,)]
+        'activation': ['relu'],           #,
+        'solver': ['adam'],                   #['adam', 'sgd'],
+    }
+    param_grid_suspeito = {
+        'hidden_layer_sizes': (12, 6), # [(50,50), (100,)]
+        'activation': ['relu'],
+        'solver': ['adam'],                   #['adam', 'sgd'],
+    }
+    return (param_grid_arma, param_grid_lugar, param_grid_suspeito)
+
+def treinar(dados, jogos_teste, chance1, chance_menos1):
+
+    max_iter = 10000000;
+    clf_arma = MLPClassifier(hidden_layer_sizes=(8, 4), activation='relu', solver='adam', alpha=0.0001, max_iter=max_iter, random_state=1)
+    clf_lugar = MLPClassifier(hidden_layer_sizes=(10, 20), activation='relu', solver='adam', alpha=0.0001, max_iter=max_iter, random_state=1)
+    clf_suspeito = MLPClassifier(hidden_layer_sizes=(12, 6), activation='relu', solver='adam', alpha=0.0001, max_iter=max_iter, random_state=1)
+
+    # clf_arma = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(8, 4), random_state=1, max_iter=1000000)
+    # clf_lugar = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(10, 5), random_state=1, max_iter=1000000)
+    # clf_suspeito = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(12, 6), random_state=1, max_iter=1000000)
+
+    param_grid_arma, param_grid_lugar, param_grid_suspeito = definir_param_grid()
+    
+    grid_search_arma = GridSearchCV(estimator=clf_arma, param_grid=param_grid_arma, n_jobs=-1, cv=3)
+    grid_search_lugar = GridSearchCV(estimator=clf_lugar, param_grid=param_grid_lugar, n_jobs=-1, cv=3)
+    grid_search_suspeito = GridSearchCV(estimator=clf_suspeito, param_grid=param_grid_suspeito, n_jobs=-1, cv=3)
+
+    arma_X, arma_y, lugar_X, lugar_y, suspeito_X, suspeito_y = gerar_dados(dados, chance1, chance_menos1)
 
     # 'Cria' ou 'faz' os treinos
     grid_search_arma.fit(arma_X, arma_y)
@@ -92,15 +105,17 @@ def treinar(entradas, jogos_teste, chance1, chance_menos1):
     grid_search_suspeito.fit(suspeito_X, suspeito_y) 
     print("Ajustou o Suspeito\n")
 
+    # # 'Cria' ou 'faz' os treinos
+    # clf_arma.fit(arma_X, arma_y)
+    # clf_lugar.fit(lugar_X, lugar_y)
+    # clf_suspeito.fit(suspeito_X, suspeito_y)
+
     # Todas as possibilidades de acertos e erros
+
     acertos = 0
-    acertos_arma = 0
-    acertos_lugar = 0
-    acertos_suspeito = 0
-    erros_totais = 0
-    acertos_arma_lugar = 0
-    acertos_arma_suspeito = 0
-    acertos_lugar_suspeito = 0
+    acertos_duplos = [0, 0, 0] # acertos_arma_lugar -> 0, acertos_arma_suspeito -> 1, acertos_lugar_suspeito -> 2
+    acertos_unitarios = [0, 0, 0] # acertos_arma -> 0,  acertos_lugar -> 1, acertos_suspeito -> 2
+    erros = 0
 
     for m in range(jogos_teste):
         crime, tabela = criar_cenario(chance1, chance_menos1)
@@ -129,20 +144,20 @@ def treinar(entradas, jogos_teste, chance1, chance_menos1):
                 if crime[2] == palpite_suspeito: # Arma, lugar e suspeito
                     acertos += 1
                 else:
-                    acertos_arma_lugar += 1
+                    acertos_duplos[0] += 1
             elif crime[2] == palpite_suspeito: # Arma e suspeito
-                acertos_arma_suspeito += 1
+                acertos_duplos[1] += 1
             else:
-                acertos_arma += 1
+                acertos_unitarios[0] += 1
         elif crime[1] == palpite_lugar: # Lugar
             if crime[2] == palpite_suspeito: # Lugar e suspeito
-                acertos_lugar_suspeito += 1
+                acertos_duplos[2] += 1
             else:
-                acertos_lugar += 1
+                acertos_unitarios[1] += 1
         elif crime[2] == palpite_suspeito: # Suspeito
-            acertos_suspeito += 1
+            acertos_unitarios[2] += 1
         else:
-            erros_totais += 1
+            erros += 1
 
     print("\n\nArma Classification Report:")
     print(classification_report(arma_y, grid_search_arma.predict(arma_X)))
@@ -159,31 +174,31 @@ def treinar(entradas, jogos_teste, chance1, chance_menos1):
     print("Confusion Matrix:")
     print(confusion_matrix(suspeito_y, grid_search_suspeito.predict(suspeito_X)))
 
-    # Vou deixar  aqui só para caso queira tirar a prova
+    # Vou deixar aqui só para caso queira tirar a prova
     string = [
-        f"\n\nCom um total de {entradas} entradas de jogos\n",
+        f"\n\nCom um total de {dados} dados de jogos\n",
         f"Para um total de {jogos_teste} testes automáticos e aleatórios\n",
         f"Com chance de {chance1}% de Corromper o 1\n",
         f"Com chance de {chance_menos1}% de Corromer o -1\n",
         f"Resultado\n",
         f"Acertou tudo: {acertos} ou {(acertos/jogos_teste)*100:.2f}%\n",
-        f"Acertou somente Arma e Lugar: {acertos_arma_lugar} ou {(acertos_arma_lugar/jogos_teste)*100:.2f}%\n",
-        f"Acertou somente Arma e Suspeito: {acertos_arma_suspeito} ou {(acertos_arma_suspeito/jogos_teste)*100:.2f}%\n",
-        f"Acertou somente Lugar e Suspeito: {acertos_lugar_suspeito} ou {(acertos_lugar_suspeito/jogos_teste)*100:.2f}%\n",
-        f"Acertou apenas a Arma: {acertos_arma} ou {(acertos_arma/jogos_teste)*100:.2f}%\n",
-        f"Acertou apenas o Lugar: {acertos_lugar} ou {(acertos_lugar/jogos_teste)*100:.2f}%\n",
-        f"Acertou apenas o Suspeito: {acertos_suspeito} ou {(acertos_suspeito/jogos_teste)*100:.2f}%\n",
-        f"Errou tudo: {erros_totais} ou {(erros_totais/jogos_teste)*100:.2f}%\n"
+        f"Acertou somente Arma e Lugar: {acertos_duplos[0]} ou {(acertos_duplos[0]/jogos_teste)*100:.2f}%\n",
+        f"Acertou somente Arma e Suspeito: {acertos_duplos[1]} ou {(acertos_duplos[1]/jogos_teste)*100:.2f}%\n",
+        f"Acertou somente Lugar e Suspeito: {acertos_duplos[2]} ou {(acertos_duplos[2]/jogos_teste)*100:.2f}%\n",
+        f"Acertou apenas a Arma: {acertos_unitarios[0]} ou {(acertos_unitarios[0]/jogos_teste)*100:.2f}%\n",
+        f"Acertou apenas o Lugar: {acertos_unitarios[1]} ou {(acertos_unitarios[1]/jogos_teste)*100:.2f}%\n",
+        f"Acertou apenas o Suspeito: {acertos_unitarios[2]} ou {(acertos_unitarios[2]/jogos_teste)*100:.2f}%\n",
+        f"Errou tudo: {erros} ou {(erros/jogos_teste)*100:.2f}%\n"
     ]
     print("".join(string))
     
     subsets = [
-        round((acertos_arma/jogos_teste)*100, 2), 
-            round((acertos_lugar/jogos_teste)*100, 2), 
-            round((acertos_arma_lugar/jogos_teste)*100, 2), 
-            round((acertos_suspeito/jogos_teste)*100, 2),
-            round((acertos_arma_suspeito/jogos_teste)*100, 2), 
-            round((acertos_lugar_suspeito/jogos_teste)*100, 2),
+        round((acertos_unitarios[0]/jogos_teste)*100, 2), 
+            round((acertos_unitarios[1]/jogos_teste)*100, 2), 
+            round((acertos_duplos[0]/jogos_teste)*100, 2), 
+            round((acertos_unitarios[2]/jogos_teste)*100, 2),
+            round((acertos_duplos[1]/jogos_teste)*100, 2), 
+            round((acertos_duplos[2]/jogos_teste)*100, 2),
             round((acertos/jogos_teste)*100, 2)
     ]
 
@@ -196,28 +211,27 @@ def treinar(entradas, jogos_teste, chance1, chance_menos1):
     
     plt.title(f"Resultado do treino (em %)")
     string = [
-        f"Total de entradas: {entradas}\n",
+        f"Total de dados: {dados}\n",
         f"Total de testes: {jogos_teste}\n",
         f"Corrupção de 1 e -1: {chance1}%, {chance_menos1}%",
     ]
     plt.text(-1, -0.8, "".join(string), fontsize=10)
-    plt.text(0.5, -0.5, f"Erros: {round((erros_totais/jogos_teste)*100, 2)}", fontsize=12)
-    plt.savefig(f'{entradas}_entradas_e_{jogos_teste}_testes_{chance1}%_para_1_e_{chance_menos1}%_para_-1')
+    plt.text(0.5, -0.5, f"Erros: {round((erros/jogos_teste)*100, 2)}", fontsize=12)
+    plt.savefig(f'{dados}_dados_e_{jogos_teste}_testes_{chance1}%_para_1_e_{chance_menos1}%_para_-1')
     # plt.show()
     plt.clf()
 
 def treino_unitario():
     print(f"\n\nCaracteristicas do Treino")
-    entradas = int(input("Quantidade de Jogos para Treinamento: "))
+    dados = int(input("Quantidade de Dados do Treinamento: "))
     jogos_teste = int(input("Quantidade de Testes: "))
     print(f"Informações adicionais")
     chance1 = int(input("Chance de Corromper o 1: "))
     chance_menos1 = int(input("Chance de Corromper o -1: "))
-    treinar(entradas, jogos_teste, chance1, chance_menos1)
+    treinar(dados, jogos_teste, chance1, chance_menos1)
 
 def main(): 
     treino_unitario()
-
 
 if __name__ == "__main__":
     main()
